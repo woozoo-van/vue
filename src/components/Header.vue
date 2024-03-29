db660b4ce4b535325920908630389003461c8442
van.woozoo <van.woozoo@gmail.com>
2024-03-29 23:47:31 +0900

    add Menu List 속성 바인딩 실습

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,12 +6,4 @@ import userList from "../assets/data/userList.json"
 
 <template>
     <h1>hello Schedule</h1>
-
-    <ul v-for="user in userList.users">
-        <li>
-            <cite>이름 : {{ user.name }}</cite>
-            <span>전화번호 : {{ user.phone_number }}</span>
-            <span>등록시간 : {{ user.created_date }}</span>
-        </li>
-    </ul>
 </template>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,21 @@
 <script setup>
+import { ref } from 'vue'
 
+import MenuList from "../assets/data/menuList.json"
+
+const activeMenuId = ref(1);
+
+function activeMenu(id) {
+    activeMenuId.value = id;
+
+}
 </script>
 
 <template>
-    <cite>nav bar</cite>
+    <ul class="menu">
+        <li v-for="menu in MenuList.menus">
+            <a :class="{ active: menu.id === activeMenuId }" @click="activeMenu(menu.id)" href="@">{{
+            menu.name }}</a>
+        </li>
+    </ul>
 </template>
