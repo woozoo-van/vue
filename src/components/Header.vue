c3e01c7bfa7e5fb06c6f973187a03d1dfc5ebc2a
van.woozoo <van.woozoo@gmail.com>
2024-03-29 22:43:49 +0900

    json data 읽기 테스트

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,17 @@
 <script setup>
 
+import userList from "../assets/data/userList.json"
+
 </script>
 
 <template>
     <h1>hello Schedule</h1>
+
+    <ul v-for="user in userList.users">
+        <li>
+            <cite>이름 : {{ user.name }}</cite>
+            <span>전화번호 : {{ user.phone_number }}</span>
+            <span>등록시간 : {{ user.created_date }}</span>
+        </li>
+    </ul>
 </template>
